1cad6bd5f40d4bea7d9e0b196b586886199e0c4b
ïfti020 <iftihaque1@gmail.com>
2026-09-15 22:52:33 +0600

    Added @OnClickListner Button & added @State that added a new to do dynamically gets added to frontend

--- 01-react-fundamentals-and-project/01-react-todo-demo/react-todos/src/App.jsx
+++ 01-react-fundamentals-and-project/01-react-todo-demo/react-todos/src/App.jsx
@@ -2,15 +2,28 @@
 
 import './App.css'
 import {TodoTable} from "./components/TodoTable.jsx";
+import {useState} from "react";
 
 function App() {
- const todos= [
+ const initialTodos= [
      {rowNumber: 1, rowDescription: 'Feed Chicken' , rowAssigned: 'User One'},
      {rowNumber: 2, rowDescription: 'Water Plants' , rowAssigned: 'User Two'},
      {rowNumber: 3, rowDescription: 'Make Dinner' , rowAssigned: 'User Three'},
      {rowNumber: 4, rowDescription: 'Clean Bedroom' , rowAssigned: 'User Four'},
      {rowNumber: 5, rowDescription: 'Get Haircut' , rowAssigned: 'User Five'}
-     ];
+     ]
+
+ const [todos, setTodos] = useState(initialTodos)
+
+ const addTodo = ()=>{
+     const newTodo = {
+         rowNumber:10,
+         rowDescription: 'new todo',
+         rowAssigned: 'User Three'
+     }
+     setTodos([...todos, newTodo])
+     console.log(todos)
+ }
 
   return (
 
@@ -21,6 +34,7 @@ function App() {
              </div>
              <div>
                 <TodoTable todos={todos}/>
+                 <button onClick={addTodo}> Click Me!</button>
              </div>
          </div>
      </div>
