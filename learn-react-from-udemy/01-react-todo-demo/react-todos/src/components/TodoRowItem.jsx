efce3404d21f9619ec378bb9b6e4ef2653063d12
ïfti020 <iftihaque1@gmail.com>
2026-09-16 11:52:43 +0600

    Add learn-react-from-udemy as normal folder

---
+++ learn-react-from-udemy/01-react-todo-demo/react-todos/src/components/TodoRowItem.jsx
@@ -0,0 +1,17 @@
+// create our first components in react
+export function TodoRowItem(props) {
+
+    const rowNumber = props.todo.rowNumber
+    const rowDescription = props.todo.rowDescription
+    const rowAssigned = props.todo.rowAssigned
+
+    return (
+        <tr onClick={() => props.deleteTodo(rowNumber)}>
+            <td>{rowNumber}</td>
+            <td>{rowDescription}</td>
+            <td>{rowAssigned}</td>
+
+        </tr>
+
+    )
+}
