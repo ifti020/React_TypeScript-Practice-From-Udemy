16480a27e970fe2ea2afb9affb4e541407b7cbca
ïfti020 <iftihaque1@gmail.com>
2026-09-16 20:55:08 +0600

    growable arrays using push

---
+++ learn-typescript-from-udemy/01-typescript-fundamentals/03-loops-and-arrays/growable-arrays.js
@@ -0,0 +1,7 @@
+"use strict";
+let sportsTwo = ["Golf", "Tenis", "Boxing"];
+sportsTwo.push("Wrestling");
+sportsTwo.push("Swimming");
+for (let tempSports of sportsTwo) {
+    console.log(tempSports);
+}
---
+++ learn-typescript-from-udemy/01-typescript-fundamentals/03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,9 @@
+let sportsTwo : string [] = ["Golf", "Tenis","Boxing"];
+
+sportsTwo.push("Wrestling");
+sportsTwo.push("Swimming");
+
+for (let tempSports of sportsTwo)
+{
+    console.log(tempSports);
+}
