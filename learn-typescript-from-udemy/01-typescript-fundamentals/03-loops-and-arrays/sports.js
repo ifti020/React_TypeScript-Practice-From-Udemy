162df35cd3850fbf167956ff2095bd3d4d1a2cb6
ïfti020 <iftihaque1@gmail.com>
2026-09-16 19:41:42 +0600

    Try simplified for loops

---
+++ learn-typescript-from-udemy/01-typescript-fundamentals/03-loops-and-arrays/sports.js
@@ -0,0 +1,15 @@
+"use strict";
+let sportsOne = ["Golf", "Cricket", "Football", "Badminton"];
+for (let i = 0; i < sportsOne.length; i++) {
+    console.log(sportsOne[i]);
+}
+console.log("\n");
+// lets use the simplied for loop
+for (let tempSport of sportsOne) {
+    if (tempSport == "Cricket") {
+        console.log(tempSport + "Iftis Favorite");
+    }
+    else {
+        console.log(tempSport);
+    }
+}
