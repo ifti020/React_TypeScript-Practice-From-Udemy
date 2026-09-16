ee1956cdd575ef4e90fbc729d6b3fcde0d2362d0
ïfti020 <iftihaque1@gmail.com>
2026-09-16 18:20:30 +0600

    Typescript variables code

---
+++ learn-typescript-from-udemy/01-typescript-fundamentals/02-variables/sample-types.js
@@ -0,0 +1,16 @@
+"use strict";
+let found = true;
+let grade = 3.22;
+let firstName = "Ifti";
+let lastName = "Haque";
+// lets break it !
+/*
+found = 0;
+grade = "A";
+firstName = false;
+*/
+console.log(found);
+console.log("The Grade is :" + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use template string use backticks\
+console.log(` ${firstName}  ${lastName} : Hello from backticks.`);
