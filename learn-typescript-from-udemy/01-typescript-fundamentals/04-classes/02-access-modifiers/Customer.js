f71c58bb89f92911583c6133e0d52d9293a8db3c
ïfti020 <iftihaque1@gmail.com>
2026-09-17 10:10:58 +0600

    Learn Access  modifiers and Getter Setter How Works

---
+++ learn-typescript-from-udemy/01-typescript-fundamentals/04-classes/02-access-modifiers/Customer.js
@@ -0,0 +1,27 @@
+"use strict";
+class Customer {
+    _firstName;
+    _lastName;
+    constructor(theFirst, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(firstName) {
+        this._firstName;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        this._lastName = value;
+    }
+}
+// lets create an instance
+let myCustomer = new Customer("Parama", "Haque");
+// myCustomer.firstName = "Parama";
+// myCustomer.lastName="Farhat";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
