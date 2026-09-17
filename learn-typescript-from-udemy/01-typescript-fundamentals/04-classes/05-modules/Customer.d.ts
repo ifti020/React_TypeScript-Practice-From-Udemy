e1b51f2ff03c8eb62ca4eaf939ffce434eb0b05f
ïfti020 <iftihaque1@gmail.com>
2026-09-17 12:09:29 +0600

    Explore Export Import

---
+++ learn-typescript-from-udemy/01-typescript-fundamentals/04-classes/05-modules/Customer.d.ts
@@ -0,0 +1,10 @@
+export declare class Customer {
+    private _firstName;
+    private _lastName;
+    constructor(_firstName: string, _lastName: string);
+    get firstName(): string;
+    set firstName(value: string);
+    get lastName(): string;
+    set lastName(value: string);
+}
+//# sourceMappingURL=Customer.d.ts.map
---
+++ learn-typescript-from-udemy/01-typescript-fundamentals/04-classes/05-modules/Driver.ts
@@ -0,0 +1,5 @@
+import {Customer} from './Customer';
+
+let myCustomer = new Customer("Ishrak","Haque");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
