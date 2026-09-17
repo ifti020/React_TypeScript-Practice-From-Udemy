c70df1023b7356418b312f46a554fa8b7ca3ab88
ïfti020 <iftihaque1@gmail.com>
2026-09-17 10:08:42 +0600

    Tsconfig

---
+++ learn-typescript-from-udemy/01-typescript-fundamentals/04-classes/03-tsconfig/Customer.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Customer {
+    _firstName;
+    _lastName;
+    constructor(theFirst, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(firstName) {
+        this._firstName;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        this._lastName = value;
+    }
+}
+// lets create an instance
+let myCustomer = new Customer("Parama", "Haque");
+// myCustomer.firstName = "Parama";
+// myCustomer.lastName="Farhat";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
+//# sourceMappingURL=Customer.js.map
