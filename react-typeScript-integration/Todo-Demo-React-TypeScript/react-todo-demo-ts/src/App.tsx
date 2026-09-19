90da70c5e110346bd5c348f52ecb0685bd00dd9c
ïfti020 <iftihaque1@gmail.com>
2026-09-19 21:36:24 +0600

    Create Directory for Todo app :React + TypeScript Integration

---
+++ react-typeScript-integration/Todo-Demo-React-TypeScript/react-todo-demo-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p> Ready for React+TypeScript Integration for our Todo app</p>
+  )
+}
+
+export default App
