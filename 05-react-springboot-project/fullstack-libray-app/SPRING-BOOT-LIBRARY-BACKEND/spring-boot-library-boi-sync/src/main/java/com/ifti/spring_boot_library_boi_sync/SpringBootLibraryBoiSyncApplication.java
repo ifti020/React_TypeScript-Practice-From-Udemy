e41dca909b96986a9a2c70b494ec3d1695fcaca8
ïfti020 <iftihaque1@gmail.com>
2026-09-24 21:14:15 +0600

    Create Backend Directory and extract Spring Initializer File for BoiSync

---
+++ 05-react-springboot-project/fullstack-libray-app/SPRING-BOOT-LIBRARY-BACKEND/spring-boot-library-boi-sync/src/main/java/com/ifti/spring_boot_library_boi_sync/SpringBootLibraryBoiSyncApplication.java
@@ -0,0 +1,13 @@
+package com.ifti.spring_boot_library_boi_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryBoiSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryBoiSyncApplication.class, args);
+	}
+
+}
---
+++ 05-react-springboot-project/fullstack-libray-app/SPRING-BOOT-LIBRARY-BACKEND/spring-boot-library-boi-sync/src/test/java/com/ifti/spring_boot_library_boi_sync/SpringBootLibraryBoiSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifti.spring_boot_library_boi_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryBoiSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
