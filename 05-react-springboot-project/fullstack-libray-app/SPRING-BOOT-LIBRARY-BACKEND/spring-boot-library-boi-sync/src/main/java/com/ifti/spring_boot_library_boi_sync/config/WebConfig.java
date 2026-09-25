7203f87aff99acbff7878cb056ed64efdeec9fd7
ïfti020 <iftihaque1@gmail.com>
2026-09-25 17:43:42 +0600

    Add WebConfig for CORS and page serialization

---
+++ 05-react-springboot-project/fullstack-libray-app/SPRING-BOOT-LIBRARY-BACKEND/spring-boot-library-boi-sync/src/main/java/com/ifti/spring_boot_library_boi_sync/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.ifti.spring_boot_library_boi_sync.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig {
+
+
+    @Value("${cors.allowedOrigin}")
+    private String allowedOrigin;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+
+            }
+        };
+    }
+
+
+
+}
