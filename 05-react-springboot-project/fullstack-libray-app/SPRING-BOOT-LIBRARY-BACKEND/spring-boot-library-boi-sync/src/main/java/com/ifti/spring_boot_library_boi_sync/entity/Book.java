0afae84dbc9ac27fe37a926e595a095448f5d9cd
ïfti020 <iftihaque1@gmail.com>
2026-09-25 12:57:21 +0600

    Added Book Entity

---
+++ 05-react-springboot-project/fullstack-libray-app/SPRING-BOOT-LIBRARY-BACKEND/spring-boot-library-boi-sync/src/main/java/com/ifti/spring_boot_library_boi_sync/entity/Book.java
@@ -0,0 +1,100 @@
+package com.ifti.spring_boot_library_boi_sync.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="author")
+    private String author;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="copies")
+    private int copies;
+
+    @Column(name="copies_Available")
+    private int copiesAvailable;
+
+    @Column(name="category")
+    private String category;
+
+    @Column(name="img")
+    private String img;
+
+
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(int copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
