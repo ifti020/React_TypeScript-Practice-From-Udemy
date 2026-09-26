350c08c0eadb34b6152cbbcbd19408b3758e6eba
ïfti020 <iftihaque1@gmail.com>
2026-09-26 12:56:50 +0600

    Create env config for API base URL and configure book service

--- 05-react-springboot-project/fullstack-libray-app/react-library-frontend/react-library-frontend/src/services/bookService.ts
+++ 05-react-springboot-project/fullstack-libray-app/react-library-frontend/react-library-frontend/src/services/bookService.ts
@@ -1,5 +1,5 @@
-import type {BookModel} from "../models/BookModel.ts";
-
+import type {BookModel} from "../models/BookModel";
+const BASE_URL= import.meta.env.VITE_API_BASE_URL;
 
 interface BookResponse {
     content : BookModel[];
@@ -13,8 +13,16 @@ export const bookService = {
     // All the Services Regarding Books
     async getBooks(pageNo: number , pageSize:number) : Promise <BookResponse>{
         const response = await fetch(
-            `http://localhost:8080/api/books?pageNo=${pageNo}&pageSize=${pageSize}`,
+            `${BASE_URL}/books?pageNo=${pageNo}&pageSize=${pageSize}`
         );
+
+        /*
+         // for debuggin pupose
+        // console.log("Request URL:", response.url);
+        // console.log("Status:", response.status);
+        // console.log("Content-Type:", response.headers.get("content-type"));
+
+         */
 
         if (!response.ok) {
             throw new Error("Failed to fetch books.");
@@ -23,5 +31,7 @@ export const bookService = {
             return  await response.json();
 
     }
+
+
 
 }
